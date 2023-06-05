44b4ef5b2019d18b8156d586e5fba50908cb65c5
lspongebobjh <lspongebobjh@gmail.com>
2023-06-05 14:06:58 +0000

    test time on v100

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #     python preprocessing/process_iwr1843.py --index $i &
 # done
 
-python preprocessing/process_iwr1843.py --index_single 5 --frame 395 &
+# python preprocessing/process_iwr1843.py --index_single 5 --frame 395 &
 # python preprocessing/process_iwr1843.py --index 2 &
 # python preprocessing/process_iwr1843.py --index 3 &
 # python preprocessing/process_iwr1843.py --index 4 &
@@ -16,4 +16,5 @@ python preprocessing/process_iwr1843.py --index_single 5 --frame 395 &
 # python main.py --config mscsa_prgcn.yaml --eval
 
 # python gen_gt.py
+python main.py RUN.debug=True RUN.use_ray=False
 
