21a4859f96c73e6f3fb454a4e4b235d1566dcc18
lspongebobjh <lspongebobjh@gmail.com>
2023-06-06 06:51:19 +0000

    update

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #     python preprocessing/process_iwr1843.py --index $i &
 # done
 
-python preprocessing/process_iwr1843.py --index_single 46 --frame 347 &
+# python preprocessing/process_iwr1843.py --index_single 46 --frame 347 &
 # python preprocessing/process_iwr1843.py --index 2 &
 # python preprocessing/process_iwr1843.py --index 3 &
 # python preprocessing/process_iwr1843.py --index 4 &
@@ -16,7 +16,7 @@ python preprocessing/process_iwr1843.py --index_single 46 --frame 347 &
 # python main.py --config mscsa_prgcn.yaml --eval
 
 # python gen_gt.py
-# HYDRA_FULL_ERROR=1
+HYDRA_FULL_ERROR=1
 
-# python main.py RUN.use_ray=True SETUP.numWorkers=4 TRAINING.epochs=30 RUN.num_ray_workers=8
+python main.py RUN.use_ray=True SETUP.numWorkers=4 TRAINING.epochs=30 RUN.num_ray_workers=8
 
