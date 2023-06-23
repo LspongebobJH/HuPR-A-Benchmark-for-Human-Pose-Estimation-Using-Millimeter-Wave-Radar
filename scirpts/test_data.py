411a768b616dbd1737d5e746405e05b62e8459ba
lspongebobjh <lspongebobjh@gmail.com>
2023-06-23 08:22:10 +0000

    finish data verification

--- scirpts/test_data.py
+++ scirpts/test_data.py
@@ -6,63 +6,49 @@ from omegaconf import OmegaConf
 
 import numpy as np
 import multiprocessing as mp
+import pickle
 
-# from tqdm.contrib import tzip
-
-# def func(path1, path2):
-#     try:
-#         np.load(path1)
-#     except Exception as e:
-#         error_list.append(path1)
-    
-#     try:
-#         np.load(path2)
-#     except Exception as e:
-#         error_list.append(path2)
-#     return error_list
-def func(c):
-    a, b = c[0], c[1]
-    return a*b
+def func(paths):
+    path1, path2 = paths[0], paths[1]
+    error_list = []
+    try:
+        np.load(path1)
+        print(f"Finish {path1}")
+    except Exception as e:
+        error_list.append(path1)
+        print(f"Error {path1}, {e}")
+    try:
+        np.load(path2)
+        print(f"Finish {path2}")
+    except Exception as e:
+        error_list.append(path2)
+        print(f"Error {path2}, {e}")
+    return error_list
 
 cfg_cmd = OmegaConf.from_cli()
 cfg = OmegaConf.load('config/mscsa_prgcn.yaml')
 cfg = OmegaConf.merge(cfg, cfg_cmd)
 
 error_list = []
+pool = mp.Pool(8)
+
+print('=== test ===')
+dataset = getDataset('test', cfg)
+res = pool.map(func, zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert))
+error_list.extend(res)
+
+print('=== train ===')
 dataset = getDataset('train', cfg)
+res = pool.map(func, zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert))
+error_list.extend(res)
 
-a = [1, 2, 3, 4]
-b = [5, 6, 7, 8]
+print('=== val ===')
+dataset = getDataset('val', cfg)
+res = pool.map(func, zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert))
+error_list.extend(res)
 
-pool = mp.Pool(4)
-# pool.map(func, zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert))
-res = pool.map(func, zip(a, b))
+pool.close()
 pool.join()
-pass
     
-
-# dataset = getDataset('val', cfg)
-# for path1, path2 in zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert):
-#     try:
-#         np.load(path1)
-#     except Exception as e:
-#         error_list.append(path1)
-    
-#     try:
-#         np.load(path2)
-#     except Exception as e:
-#         error_list.append(path2)
-
-# dataset = getDataset('test', cfg)
-# for path1, path2 in zip(dataset.VRDAEPaths_hori, dataset.VRDAEPaths_vert):
-#     try:
-#         np.load(path1)
-#     except Exception as e:
-#         error_list.append(path1)
-    
-#     try:
-#         np.load(path2)
-#     except Exception as e:
-#         error_list.append(path2)
-
-pass
+with open('error_list.pkl', 'wb') as f:
+    pickle.dump(error_list, f)
